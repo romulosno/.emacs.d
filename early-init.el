d3cf9e756cd1ff55ee8be8e199bd72f827538b1f
Romulo Souza <romulosnoliveira@gmail.com>
2023-08-15 08:27:59 -0300

    Add better defaults

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(setq gc-cons-threshold 6400000)
+
+(setq default-frame-alist
+      '((height . 35)
+	(width . 100)
+	(tooltip . nil)
+	(tool-bar-lines . nil)
+	(menu-bar-lines . nil)
+	(vertical-scroll-bars . nil)
+	(horizontal-scroll-bars . nil)))
--- init.el
+++ init.el
@@ -1,47 +1,67 @@
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-(load-theme 'wombat)
-
-(setq default-frame-alist
-      '((height . 35)
-	(width . 100)
-	(tooltip . nil)
-	(tool-bar-lines . nil)
-	(menu-bar-lines . nil)
-	(vertical-scroll-bars . nil)
-	(horizontal-scroll-bars . nil)))
-
 (when (member "Iosevka Comfy" (font-family-list))
   (add-to-list 'default-frame-alist '(font . "Iosevka Comfy-11")))
 
-(electric-pair-mode)
-(repeat-mode)
-(savehist-mode)
-(save-place-mode)
+(setq package-quickstart t)
 
-(setq tab-always-indent 'complete
-      completion-auto-help 'visible)
-
-(setq inhibit-startup-screen t
-      inhibit-startup-message t)
-
-(global-set-key (kbd "C-z") #'repeat)
+(with-eval-after-load 'flymake
+  (bind-keys
+   ("C-z" . repeat)
+   ("C-c a" . org-agenda)
+   ("C-c o" . outline-minor-mode)))
 
 (with-eval-after-load 'dired
   (require 'dired-x))
 
 (with-eval-after-load 'flymake
-  (define-key flymake-mode-map (kbd "M-n") #'flymake-goto-next-error)
-  (define-key flymake-mode-map (kbd "M-p") #'flymake-goto-prev-error)
-  (define-key flymake-mode-map (kbd "C-c c l") #'flymake-show-buffer-diagnostics))
+  (bind-keys
+   :map flymake-mode-map
+   ("M-n" . flymake-goto-next-error)
+   ("M-p" . flymake-goto-prev-error)
+   ("C-c c l" . flymake-show-buffer-diagnostics)))
 
 (with-eval-after-load 'eglot
-  (define-key eglot-mode-map (kbd "C-c c f") #'eglot-format)
-  (define-key eglot-mode-map (kbd "C-c c a") #'eglot-code-actions)
-  (define-key eglot-mode-map (kbd "C-c c s") #'eglot-shutdown)
-  (define-key eglot-mode-map (kbd "C-c c r") #'eglot-rename)
+  (bind-keys
+   :map eglot-mode-map
+   ("C-c c f" . eglot-format)
+   ("C-c c a" . eglot-code-actions)
+   ("C-c c s" . eglot-shutdown)
+   ("C-c c r" . eglot-rename))
 
   (cl-defmethod eglot-execute-command
     (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
     "Eclipse JDT breaks spec and replies with edits as arguments."
     (mapc #'eglot--apply-workspace-edit arguments)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(completion-auto-help 'visible)
+ '(completion-show-help nil)
+ '(completions-max-height 15)
+ '(custom-enabled-themes '(modus-vivendi))
+ '(custom-safe-themes t)
+ '(delete-selection-mode t)
+ '(dired-listing-switches "-alh --group-directories-first")
+ '(electric-pair-mode t)
+ '(inhibit-startup-screen t)
+ '(kill-whole-line t)
+ '(modus-themes-mode-line '(accented borderless))
+ '(org-use-speed-commands t)
+ '(outline-minor-mode-prefix [134217839])
+ '(package-quickstart t t)
+ '(package-selected-packages '(markdown-mode gruvbox-theme))
+ '(repeat-mode t)
+ '(ring-bell-function 'ignore)
+ '(save-place-mode t)
+ '(savehist-mode t)
+ '(tab-always-indent 'complete)
+ '(tab-bar-select-tab-modifiers '(meta)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
