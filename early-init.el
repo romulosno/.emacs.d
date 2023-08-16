be305ab179662a61909c2c312ae1e5cf2011d844
Romulo Souza <romulosnoliveira@gmail.com>
2023-08-16 08:36:25 -0300

    Adjust

--- early-init.el
+++ early-init.el
@@ -2,9 +2,9 @@
 
 (setq default-frame-alist
       '((height . 35)
-	(width . 100)
-	(tooltip . nil)
-	(tool-bar-lines . nil)
-	(menu-bar-lines . nil)
-	(vertical-scroll-bars . nil)
-	(horizontal-scroll-bars . nil)))
+       (width . 100)
+       (tooltip . nil)
+       (tool-bar-lines . nil)
+       (menu-bar-lines . nil)
+       (vertical-scroll-bars . nil)
+       (horizontal-scroll-bars . nil)))
--- init.el
+++ init.el
@@ -1,32 +1,39 @@
 (when (member "Iosevka Comfy" (font-family-list))
-  (add-to-list 'default-frame-alist '(font . "Iosevka Comfy-11")))
+  (add-to-list 'default-frame-alist
+	       '(font . "Iosevka Comfy-11")))
 
-(setq package-quickstart t)
+(defun sort-by-alpha-length (elems)
+  "Sort ELEMS first alphabetically, then by length."
+  (sort elems (lambda (c1 c2)
+                (or (string-version-lessp c1 c2)
+                    (< (length c1) (length c2))))))
 
-(with-eval-after-load 'flymake
-  (bind-keys
-   ("C-z" . repeat)
-   ("C-c a" . org-agenda)
-   ("C-c o" . outline-minor-mode)))
+(defun sort-by-history (elems)
+  "Sort ELEMS by minibuffer history."
+  (if-let ((hist (and (not (eq minibuffer-history-variable t))
+                      (symbol-value minibuffer-history-variable))))
+      (minibuffer--sort-by-position hist elems)
+    (sort-by-alpha-length elems)))
+
+(global-set-key (kbd "<f5>") #'org-capture)
+(global-set-key (kbd "C-z") #'repeat)
+(global-set-key (kbd "C-c a") #'org-agenda-list)
+(global-set-key (kbd "C-c o") #'outline-minor-mode)
+(define-key completion-list-mode-map (kbd "s") #'isearch-forward)
 
 (with-eval-after-load 'dired
   (require 'dired-x))
 
 (with-eval-after-load 'flymake
-  (bind-keys
-   :map flymake-mode-map
-   ("M-n" . flymake-goto-next-error)
-   ("M-p" . flymake-goto-prev-error)
-   ("C-c c l" . flymake-show-buffer-diagnostics)))
+  (define-key flymake-mode-map (kbd "M-n") #'flymake-goto-next-error)
+  (define-key flymake-mode-map (kbd "M-p") #'flymake-goto-prev-error)
+  (define-key flymake-mode-map (kbd "C-c c l") #'flymake-show-buffer-diagnostics))
 
 (with-eval-after-load 'eglot
-  (bind-keys
-   :map eglot-mode-map
-   ("C-c c f" . eglot-format)
-   ("C-c c a" . eglot-code-actions)
-   ("C-c c s" . eglot-shutdown)
-   ("C-c c r" . eglot-rename))
-
+  (define-key eglot-mode-map (kbd "C-c c f") #'eglot-format)
+  (define-key eglot-mode-map (kbd "C-c c a") #'eglot-code-actions)
+  (define-key eglot-mode-map (kbd "C-c c s") #'eglot-shutdown)
+  (define-key eglot-mode-map (kbd "C-c c r") #'eglot-rename)
   (cl-defmethod eglot-execute-command
     (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
     "Eclipse JDT breaks spec and replies with edits as arguments."
@@ -37,21 +44,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(completion-auto-help 'visible)
+ '(completion-auto-help 'always)
+ '(completion-auto-select 'second-tab)
  '(completion-show-help nil)
  '(completions-max-height 15)
- '(custom-enabled-themes '(modus-vivendi))
+ '(completions-sort 'sort-by-history)
+ '(custom-enabled-themes '(wombat))
  '(custom-safe-themes t)
  '(delete-selection-mode t)
  '(dired-listing-switches "-alh --group-directories-first")
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(electric-pair-mode t)
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
  '(modus-themes-mode-line '(accented borderless))
+ '(org-capture-templates
+   '(("n" "Nota" entry
+      (file+headline "~/org/notes.org" "Notas")
+      "* %?\12%i\12%a" :kill-buffer t)
+     ("t" "Task" entry
+      (file+headline "~/org/notes.org" "Tasks")
+      "* TODO %?\12  %i\12  %a" :kill-buffer t)))
  '(org-use-speed-commands t)
  '(outline-minor-mode-prefix [134217839])
- '(package-quickstart t t)
- '(package-selected-packages '(markdown-mode gruvbox-theme))
+ '(package-quickstart t)
+ '(package-selected-packages '(yaml-mode markdown-mode gruvbox-theme))
  '(repeat-mode t)
  '(ring-bell-function 'ignore)
  '(save-place-mode t)
