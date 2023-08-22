ce1b45d03d3be91f3635259d5b49700e339bc029
Romulo Souza <romulosnoliveira@gmail.com>
2023-08-21 22:36:33 -0300

    Adjust to better defaults

--- early-init.el
+++ early-init.el
@@ -1,10 +1,19 @@
-(setq gc-cons-threshold 6400000)
+(defvar default-gc-cons-threshold gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
 
-(setq default-frame-alist
-      '((height . 35)
-       (width . 100)
-       (tooltip . nil)
-       (tool-bar-lines . nil)
-       (menu-bar-lines . nil)
-       (vertical-scroll-bars . nil)
-       (horizontal-scroll-bars . nil)))
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(modify-all-frames-parameters '((height . 35)
+				(width . 100)))
+
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t
+      use-file-dialog nil
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold default-gc-cons-threshold)) t)
--- init.el
+++ init.el
@@ -1,26 +1,29 @@
+;;;; Customize
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror)
+
+;;;; Font and theme
 (when (member "Iosevka Comfy" (font-family-list))
-  (add-to-list 'default-frame-alist
-	       '(font . "Iosevka Comfy-11")))
+  (modify-all-frames-parameters '((font . "Iosevka Comfy-11"))))
+(load-theme 'modus-vivendi t)
 
-(defun sort-by-alpha-length (elems)
-  "Sort ELEMS first alphabetically, then by length."
-  (sort elems (lambda (c1 c2)
-                (or (string-version-lessp c1 c2)
-                    (< (length c1) (length c2))))))
+;;;; Modes
+(delete-selection-mode t)
+(electric-pair-mode t)
+(save-place-mode t)
+(savehist-mode t)
+(repeat-mode t)
 
-(defun sort-by-history (elems)
-  "Sort ELEMS by minibuffer history."
-  (if-let ((hist (and (not (eq minibuffer-history-variable t))
-                      (symbol-value minibuffer-history-variable))))
-      (minibuffer--sort-by-position hist elems)
-    (sort-by-alpha-length elems)))
-
+;;;; Keybindings
 (global-set-key (kbd "<f5>") #'org-capture)
 (global-set-key (kbd "C-z") #'repeat)
 (global-set-key (kbd "C-c a") #'org-agenda-list)
 (global-set-key (kbd "C-c o") #'outline-minor-mode)
+(global-set-key (kbd "M-s i") #'imenu)
+(global-set-key (kbd "C-c k") #'kill-current-buffer)
 (define-key completion-list-mode-map (kbd "s") #'isearch-forward)
 
+;;;; Lazy loading
 (with-eval-after-load 'dired
   (require 'dired-x))
 
@@ -38,48 +41,3 @@
     (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
     "Eclipse JDT breaks spec and replies with edits as arguments."
     (mapc #'eglot--apply-workspace-edit arguments)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(completion-auto-help 'always)
- '(completion-auto-select 'second-tab)
- '(completion-show-help nil)
- '(completions-max-height 15)
- '(completions-sort 'sort-by-history)
- '(custom-enabled-themes '(wombat))
- '(custom-safe-themes t)
- '(delete-selection-mode t)
- '(dired-listing-switches "-alh --group-directories-first")
- '(ediff-split-window-function 'split-window-horizontally)
- '(ediff-window-setup-function 'ediff-setup-windows-plain)
- '(electric-pair-mode t)
- '(inhibit-startup-screen t)
- '(kill-whole-line t)
- '(modus-themes-mode-line '(accented borderless))
- '(org-capture-templates
-   '(("n" "Nota" entry
-      (file+headline "~/org/notes.org" "Notas")
-      "* %?\12%i\12%a" :kill-buffer t)
-     ("t" "Task" entry
-      (file+headline "~/org/notes.org" "Tasks")
-      "* TODO %?\12  %i\12  %a" :kill-buffer t)))
- '(org-use-speed-commands t)
- '(outline-minor-mode-prefix [134217839])
- '(package-quickstart t)
- '(package-selected-packages '(yaml-mode markdown-mode gruvbox-theme))
- '(repeat-mode t)
- '(ring-bell-function 'ignore)
- '(save-place-mode t)
- '(savehist-mode t)
- '(tab-always-indent 'complete)
- '(tab-bar-select-tab-modifiers '(meta)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
