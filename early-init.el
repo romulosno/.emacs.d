72c1fb58f0c41c28511e97ee84cf9bc4ab9e4f95
Romulo Souza <romulosnoliveira@gmail.com>
2024-01-20 19:51:49 -0300

    Update

--- early-init.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
-(add-to-list 'default-frame-alist '(background-color . "#edede8"))
-
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
--- init.el
+++ init.el
@@ -1,31 +1,16 @@
-(setq display-buffer-mark-dedicated t)
-(add-to-list 'display-buffer-alist
-	     '("\\*Occur\\*"
-	       (display-buffer-below-selected)
-	       (dedicated . t)
-	       (height . 0.2)))
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+(add-to-list 'default-frame-alist '(background-color . "black"))
+(add-to-list 'default-frame-alist '(foreground-color . "white"))
 
-(add-to-list 'display-buffer-alist
-	     '("*Async Shell Command*"
-	       (display-buffer-in-side-window)
-	       (side . bottom)
-	       (slot . 0)
-	       (height . 0.3)))
+(set-face-attribute 'mode-line nil :background "gray15" :foreground "white")
 
-(add-to-list 'display-buffer-alist
-	     '("\\*\\(Help\\|info\\|eldoc\\|Org Agenda\\)\\*"
-	       (display-buffer-reuse-mode-window display-buffer-pop-up-frame)
-	       (dedicated . t)))
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(electric-pair-mode)
-(savehist-mode)
-(save-place-mode)
-(column-number-mode)
-(repeat-mode)
-(delete-selection-mode)
+(electric-pair-mode 1)
+(savehist-mode 1)
+(save-place-mode 1)
 
 (add-hook 'org-mode-hook #'abbrev-mode)
 (add-hook 'prog-mode-hook #'abbrev-mode)
@@ -33,25 +18,20 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file t)
 
-(setq backup-directory-alist '(("." . "~/.backups/")))
-
-(setq kill-whole-line t)
 (setq inhibit-startup-screen t)
+(setq read-process-output-max (* 1024 1024))
+(setq org-use-speed-commands t)
+(setq org-confirm-babel-evaluate nil)
+(setq use-short-answers t)
+(setq kill-whole-line t)
 (setq visible-bell t)
-
-(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-
+(setq enable-local-variables :all)
+(setq project-mode-line t)
 (setq tab-always-indent 'complete)
-(add-to-list 'completion-styles 'flex t)
 
-(put 'narrow-to-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-
-(global-set-key (kbd "C-c k") #'kill-current-buffer)
-(global-set-key (kbd "C-c f") #'ffap)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-z") #'repeat)
+(global-set-key "\C-z" #'repeat)
+(global-set-key "\C-ca" #'org-agenda)
+(global-set-key "\C-cl" #'org-store-link)
 
 (with-eval-after-load 'flymake
   (define-key flymake-mode-map (kbd "M-n") #'flymake-goto-next-error)
@@ -60,8 +40,4 @@
 (with-eval-after-load 'eglot
   (define-key eglot-mode-map (kbd "C-c c a") #'eglot-code-actions)
   (define-key eglot-mode-map (kbd "C-c c r") #'eglot-rename)
-  (define-key eglot-mode-map (kbd "C-c c f") #'eglot-format)
-  (cl-defmethod eglot-execute-command
-    (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
-    "Eclipse JDT breaks spec and replies with edits as arguments."
-    (mapc #'eglot--apply-workspace-edit arguments)))
+  (define-key eglot-mode-map (kbd "C-c c f") #'eglot-format))
