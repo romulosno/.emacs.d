f50eba4be052340e0e62414c3af19e423d1e77d3
Romulo Souza <romulosnoliveira@gmail.com>
2024-01-21 19:00:05 -0300

    Update

--- init.el
+++ init.el
@@ -1,8 +1,17 @@
-(add-to-list 'default-frame-alist '(font . "Hack-10.5"))
-(add-to-list 'default-frame-alist '(background-color . "gray10"))
-(add-to-list 'default-frame-alist '(foreground-color . "ivory"))
-(set-face-attribute 'mode-line nil :background "gray20" :foreground "ivory")
-(set-face-attribute 'tab-bar nil :inherit 'default)
+(add-to-list 'default-frame-alist '(background-color . "#16191c"))
+(add-to-list 'default-frame-alist '(foreground-color . "white")))
+(when (member ("Hack" (font-family-list)))
+  (add-to-list 'default-frame-alist '(font . "Hack-11")))
+
+(set-face-attribute 'mode-line nil
+		    :background "gray20"
+		    :foreground "white")
+(set-face-attribute 'tab-bar nil
+		    :inherit 'default
+		    :background "gray20"
+		    :foreground "white")
+(set-face-attribute 'tab-bar-tab-inactive nil
+		    :background "gray10")
 
 (require 'dired-x)
 (require 'ffap)
@@ -21,15 +30,20 @@
 
 (setq inhibit-startup-screen t)
 (setq read-process-output-max (* 1024 1024))
-(setq org-use-speed-commands t)
-(setq org-confirm-babel-evaluate nil)
 (setq use-short-answers t)
 (setq kill-whole-line t)
 (setq visible-bell t)
 (setq enable-local-variables :all)
 (setq project-mode-line t)
-(setq tab-always-indent 'complete)
-(setq completion-show-help nil)
+
+(setq org-use-speed-commands t
+      org-confirm-babel-evaluate nil)
+
+(setq tab-always-indent 'complete
+      completion-show-help nil)
+
+(setq tab-bar-select-tab-modifiers '(meta)
+      tab-bar-show 1)
 
 (global-set-key "\C-z" #'repeat)
 (global-set-key "\C-ca" #'org-agenda)
