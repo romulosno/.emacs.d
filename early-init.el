034648a6952422a41db98c134bff22f747392ac7
romulosno <romulosnoliveira@gmail.com>
2024-02-20 23:26:31 -0300

    Update

--- early-init.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
-(add-to-list 'default-frame-alist '(background-color . "#e0ddce"))
-
-(menu-bar-mode 0)
-(tool-bar-mode 0)
--- init.el
+++ init.el
@@ -1,37 +1,32 @@
-(require 'dired-x)
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(indent-tabs-mode 0)
 
-(set-face-attribute 'fringe nil :background "#e0ddce")
-(set-face-attribute 'variable-pitch nil :font "Liberation Serif-11")
-(set-face-attribute 'fixed-pitch nil :font "DejaVu Sans Mono-10")
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file t)
-
-(electric-pair-mode 1)
-(savehist-mode 1)
-(save-place-mode 1)
 (delete-selection-mode 1)
-
-(setq repeat-exit-key "RET")
+(electric-pair-mode 1)
+(save-place-mode 1)
+(savehist-mode 1)
 (repeat-mode 1)
+
+(setq-default truncate-lines t)
+(setq-default tab-width 4)
+
+(setq tab-always-indent 'complete)
+(setq js-indent-level 2)
 
 (setq inhibit-startup-screen t)
 (setq kill-whole-line t)
-(setq visible-bell t)
+(setq repeat-exit-key "RET")
 (setq set-mark-command-repeat-pop t)
+(setq use-short-answers t)
+(setq visible-bell t)
 
-(setq-default truncate-lines t)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq js-indent-level 2)
+(setq tab-bar-show 1)
+(setq tab-bar-close-button-show nil
+	  tab-bar-new-button-show nil)
 
-(setq tab-always-indent 'complete)
-(setq completion-show-help nil)
-(add-to-list 'completion-styles 'flex t)
-
-(defalias 'yes-or-no-p 'y-or-n-p)
+(require 'dired-x)
 
 (add-hook 'prog-mode-hook #'abbrev-mode)
 (add-hook 'org-mode-hook #'abbrev-mode)
@@ -40,3 +35,24 @@
 (global-set-key "\C-ch" #'global-hl-line-mode)
 (global-set-key "\C-ca" #'org-agenda)
 (global-set-key "\C-z" #'repeat)
+
+(custom-theme-set-faces
+ 'user
+ '(cursor ((t (:background "#447c80"))))
+ '(default ((t (:background "#072933" :foreground "#e6e6d1" :font "Hack-11"))))
+ '(diff-refine-added ((t (:background "#164508"))))
+ '(diff-added ((t (:background "#123308"))))
+ '(diff-removed ((t (:background "#380c04"))))
+ '(diff-refine-removed ((t (:background "#4a150b"))))
+ '(font-lock-keyword-face ((t (:foreground "#daa520"))))
+ '(font-lock-string-face ((t (:foreground "#a0b6bd"))))
+ '(font-lock-comment-face ((t (:foreground "#7c909c"))))
+ '(font-lock-constant-face ((t (:foreground "#9bbae0"))))
+ '(font-lock-builtin-face ((t (:foreground "#ffa07a"))))
+ '(fringe ((t (:background "#0f2b33"))))
+ '(mode-line ((t (:background "#454737"))))
+ '(region ((t (:background "#153a45"))))
+ '(tab-bar ((t (:background "#454737"))))
+ '(tab-bar-tab-inactive ((t (:background "#484a3d" :foreground "#c2c2ba")))))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
