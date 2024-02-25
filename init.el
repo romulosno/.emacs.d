552b0ee55349c7151d81ddbeb0de3a4fda7b5188
romulosno <romulosnoliveira@gmail.com>
2024-02-25 20:15:42 -0300

    Update

--- init.el
+++ init.el
@@ -7,26 +7,34 @@
 (electric-pair-mode 1)
 (save-place-mode 1)
 (savehist-mode 1)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
+
+(setq repeat-exit-key "RET")
 (repeat-mode 1)
 
 (setq-default truncate-lines t)
 (setq-default tab-width 4)
-
-(setq tab-always-indent 'complete)
 (setq js-indent-level 2)
 
-(setq inhibit-startup-screen t)
-(setq kill-whole-line t)
-(setq repeat-exit-key "RET")
-(setq set-mark-command-repeat-pop t)
-(setq use-short-answers t)
-(setq visible-bell t)
+(setq tab-always-indent 'complete
+	  completion-show-help nil
+	  completions-format 'one-column)
 
-(setq tab-bar-show 1)
+(setq inhibit-startup-screen t
+	  kill-whole-line t
+	  set-mark-command-repeat-pop t
+	  use-short-answers t
+	  visible-bell t
+	  view-read-only t)
+
 (setq tab-bar-close-button-show nil
-	  tab-bar-new-button-show nil)
+	  tab-bar-new-button-show nil
+	  tab-bar-show 1)
 
-(require 'dired-x)
+(with-eval-after-load 'dired
+  (require 'dired-x))
 
 (add-hook 'prog-mode-hook #'abbrev-mode)
 (add-hook 'org-mode-hook #'abbrev-mode)
@@ -34,12 +42,18 @@
 (global-set-key "\C-ck" #'kill-current-buffer)
 (global-set-key "\C-ch" #'global-hl-line-mode)
 (global-set-key "\C-ca" #'org-agenda)
+(global-set-key "\C-cl" #'org-store-link)
+(global-set-key "\C-cd" #'delete-pair)
+(global-set-key "\C-cs" #'scroll-lock-mode)
 (global-set-key "\C-z" #'repeat)
+(global-set-key "\M-u" #'upcase-dwim)
+(global-set-key "\M-l" #'downcase-dwim)
+(global-set-key "\M-c" #'capitalize-dwim)
 
 (custom-theme-set-faces
  'user
  '(cursor ((t (:background "#447c80"))))
- '(default ((t (:background "#0c2d36" :foreground "#e6e6d1" :font "Hack-11"))))
+ '(default ((t (:background "#0c2830" :foreground "#e6e6d1" :font "Hack-11"))))
  '(diff-refine-added ((t (:background "#164508"))))
  '(diff-added ((t (:background "#123308"))))
  '(diff-removed ((t (:background "#380c04"))))
@@ -50,9 +64,8 @@
  '(font-lock-constant-face ((t (:foreground "#9bbae0"))))
  '(font-lock-builtin-face ((t (:foreground "#ffa07a"))))
  '(fringe ((t (:background "#0c2830"))))
- '(mode-line ((t (:background "#5a5c4a"))))
+ '(mode-line ((t (:background "#214e6e"))))
  '(region ((t (:background "#153a45"))))
  '(tab-bar ((t (:background "#5a5c4a"))))
  '(tab-bar-tab-inactive ((t (:background "#484a3d" :foreground "#c2c2ba")))))
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
